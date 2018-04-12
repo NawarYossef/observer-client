6567ffc949b57ded814e160c1346c418e993884f
nawar yossef <nawaryossef2@gmail.com>
2018-04-12 18:34:35 -0500

    add 'about-me' and 'examples' sections for the landing page

--- src/containers/landing-page.js
+++ src/containers/landing-page.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import JobIcon from "../images/job-icon.png"
+import LaptopIcon from "../images/laptop-icon.png"
+import BagIcon from "../images/bag-icon.png"
+import CalendarImg from "../images/calendar-img.png"
+import PaperImg from "../images/paper-icon.png"
+import headerIcon from "../images/header-icon.png"
+
 import "./styles/landing-page.css";
 
 export default class LandingPage extends Component {
@@ -10,7 +17,7 @@ export default class LandingPage extends Component {
       <div>
         <section className="header-content-section">
           <aside>
-            <h2>Manage your job search and professional contacts effectively.</h2>
+            <h4>Manage your job search and professional contacts effectively.</h4>
             <h3>Finding  Job Can Be Simple With Observer</h3>
             <div className="links-wrapper">
               <button className="link-to-signup">
@@ -28,13 +35,56 @@ export default class LandingPage extends Component {
         <main>
           <section className="about">
             <h2>This is how it works!</h2>
-            <div className="jobs-text text">
-              <h3>Save jobs details</h3>
-              <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque</p>
+
+            <div className="description">
+              <div className="img-wrapper">
+                <img src={JobIcon} alt="icon" />
+                <h5>Keep track of your contacts</h5>
+              </div>
+              <div className="img-wrapper">
+                <img src={BagIcon} alt="icon" />
+                <h5>Save your job information</h5>
+              </div>
+              <div className="img-wrapper">
+                <img src={LaptopIcon} alt="icon" />
+                <h5>Manage your activities</h5>
+              </div>
+            </div>
+
+            <section className="description-section">
+              <div className="text-wrapper left">
+                <h6>Manage your activities</h6>
+                <p className="calendar-text">Use our calendar to save and access all your activities on any giving day</p>
+              </div>
+              <div className="img-wrapper right">
+                <img src={CalendarImg} alt="icon" />
+              </div>
+            </section>
+          </section>
+
+          <section className="description-section join-us">
+            <div className="img-wrapper left">
+              <img src={PaperImg} alt="icon" />
+            </div>
+            <div className="text-wrapper right">
+              <p className="join-us-text">Join us today and enjoy preparing and planning for your next job. </p>
+              <button className="link-to-signup with-border">
+                <Link to="/signup">
+                  Get started for free!
+                </Link>
+              </button>
             </div>
           </section>
         </main>
+        
       </div>
     )
   }
 }
+
+// <footer>
+//           <div>
+//             <img src={headerIcon} alt="logo" />
+//             <h1 className="App-title">OBSERVER</h1>
+//           </div>
+//         </footer>
