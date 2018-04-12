376374e9a474551dd33a73328d96c4bae795abff
nawar yossef <nawaryossef2@gmail.com>
2018-04-12 18:53:01 -0500

    import correct file name into test file. Edit file name for consistency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter from "./containers/appRouter";
+import AppRouter from "./containers/app-router";
 import store from "./store";
 
 ReactDOM.render(
