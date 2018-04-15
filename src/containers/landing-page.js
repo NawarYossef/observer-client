a8cc018056d7bb2f5c4a6253ee5ca97074818d61
nawar yossef <nawaryossef2@gmail.com>
2018-04-14 22:54:43 -0500

    add media query for landing page

--- src/containers/landing-page.js
+++ src/containers/landing-page.js
@@ -9,6 +9,7 @@ import PaperImg from "../images/paper-icon.png"
 import headerIconBlack from "../images/header-icon-black.png"
 
 import "./styles/landing-page.css";
+import FontAwesome from 'react-fontawesome'
 
 export default class LandingPage extends Component {
 
@@ -81,12 +82,29 @@ export default class LandingPage extends Component {
             <img src={headerIconBlack} alt="icon" />
             <h1 className="App-title">OBSERVER</h1>
           </div>
-          <h4 id="made-with">
-            Made with
-        <i class="fa fa-heart fa created-by-icon" aria-hidden="true"></i>
-            and
-        <i class="fa fa-coffee fa created-by-icon" aria-hidden="true"></i>
-          </h4>
+          <div className="contacts col-12">
+            <ul className="">
+              <li className="">
+                <a href="https://www.linkedin.com/in/nawar-yossef-78924211a/" target="_blank" rel="noopener noreferrer" name="linkedin" role="presentation">
+                  <FontAwesome name="linkedin" />
+                </a>
+              </li>
+              <li className="">
+                <a href="https://github.com/NawarYossef" target="_blank" rel="noopener noreferrer" name="github">
+                  <FontAwesome name="github" />
+                </a>
+              </li>
+              <li className="">
+                <a href="http://www.nawaryossef.com/" rel="noopener noreferrer" name="personal-website">
+                  <FontAwesome name="shopping-bag" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <p className="copyright">
+            <FontAwesome name="copyright" />
+            <span>2018</span> Nawar Yossef
+          </p>
         </footer>
       </div>
     )
